89638910058cd64441f95baa555b87cf085d91e7
latansa123 <latansatahsya@gmail.com>
2024-08-29 14:32:51 +0700

    menambahkan on resume

---
+++ app/src/main/java/com/ubaya/native160421149/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ubaya.native160421149
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("aaaaa")
+    }
+}
