8368496b172aa59b2eb44ac59605598ff5dae5b7
latansa123 <latansatahsya@gmail.com>
2024-08-29 14:48:11 +0700

    menambahkan login activity

---
+++ app/src/main/java/com/ubaya/native160421149/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.native160421149
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
